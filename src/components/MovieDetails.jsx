1a4831ca4750d87fbcdab8b94006c914e2c0b352
FERKIOUI Akram <aa.ferkioui@ensta.edu.dz>
2025-09-12 02:23:40 +0100

    feat: Add "Watch Trailer" button to MovieDetails component for direct access to movie trailers

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -213,6 +213,21 @@ export default function MovieDetails({ moviesId, onClose }) {
 
                     {/* Buttons */}
                     <div className="mt-8 flex flex-wrap gap-3">
+                      <a href={`https://www.youtube.com/results?search_query=${movie.title}+trailer`} target="_blank" className="bg-purple-600 hover:bg-purple-700 text-white px-6 py-3 rounded-lg flex items-center gap-2 transition-colors">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        Watch Trailer
+                      </a>
                       <a
                         href={`https://www.themoviedb.org/movie/${movie.id}`}
                         target="_blank"
